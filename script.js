2f39ca54c5e81946d6fb982ab97d3d9aac05b60b
srimythri1018 <115878123+srimythri1018@users.noreply.github.com>
2023-06-16 15:21:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
 //your JS code here. If required.
+// Get the necessary elements
+const playersForm = document.getElementById('playersForm');
+const player1Input = document.getElementById('player-1');
+const player2Input = document.getElementById('player-2');
+const submitButton = document.getElementById('submit');
+const board = document.getElementById('board');
+const cells = document.querySelectorAll('.cell');
+const message = document.querySelector('.message');
+
+let currentPlayer = 'Player 1';
+
+// Event listener for submit button
+submitButton.addEventListener('click', function () {
+  const player1 = player1Input.value;
+  const player2 = player2Input.value;
+
+  if (player1 && player2) {
+    playersForm.style.display = 'none';
+    board.style.display = 'block';
+    message.textContent = `${player1}, you're up!`;
+  }
+});
+
+// Event listener for cell click
+cells.forEach(function (cell) {
+  cell.addEventListener('click', function () {
+    if (this.textContent === '') {
+      this.textContent = currentPlayer === 'Player 1' ? 'X' : 'O';
+      message.textContent = currentPlayer === 'Player 1' ? `${player2}, you're up!` : `${player1}, you
